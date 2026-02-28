86948b3d0d0b3a147eb5c0cffeaf085787573247
bwheeler-nmsu <97681002+bwheeler-nmsu@users.noreply.github.com>
2026-02-28 10:20:59 -0700

    Update terradactylutils3.R
    removing create species list function

--- R/terradactylutils3.R
+++ R/terradactylutils3.R
@@ -7,97 +7,6 @@ use_package("tidyselect")
 use_package("magrittr")
 
 
-
-
-###############################################
-#' Create species list from DIMA Tables
-#'
-#'Creates a species list using the information from species tables in the DIMA
-#'
-#' @param tblSpeciesGeneric as a data.frame tblSpeciesGeneric from DIMA tables
-#' @param tblSpecies as a data.frame tblSpecies from DIMA tables
-#' @param projectkey list of the unique ProjectKeys, which can be found in the dataHeader created using terradactylutils2::create_header()
-#' @param path_species_main path to where you save your species lists
-#' @param species_list_NOT_created T when a species list has not been created; F if user already has a species list for geoindicator calculations
-#' @param USDA_plants a data.frame of the USDA plants with the 4 letter code, GrowthHabit and Duration
-#' @param speciescode the column name in the USDA plant list file that contains the four letter codes
-#'
-#' @return CSV(s) of a species list for each ProjectKey provided by the user
-#' @export
-#'
-#' @examples create_species_list(species_list_NOT_created = T, tblSpeciesGeneric = tblSpeciesGeneric, tblSpecies = tblSpecies, projectkey = unique(dataHeader$ProjectKey), path_species_main = paste0("D:/data_preparation_docs_used_from_06012024_04302025/data_preparation_docs_used_from_06012024_04302025/Docs for data prep/Data/species_lists/"), USDA_plants = read.csv(USDA_plants.csv), speciescode = "UpdatedSpeciesCode")
-create_species_list <- function(species_list_NOT_created,tblSpeciesGeneric, tblSpecies, projectkey, path_species_main, USDA_plants, speciescode){
-if(species_list_NOT_created){
-  woody_codes <- c(1:4)
-  tblSpeciesGeneric$GrowthHabit <- ifelse(tblSpeciesGeneric$GrowthHabitCode %in% woody_codes, "Woody",
-                                          "NonWoody")
-
-  tblSpeciesGeneric$GrowthHabitSub <- ifelse(tblSpeciesGeneric$GrowthHabitCode ==1, "Tree",
-                                             ifelse(tblSpeciesGeneric$GrowthHabitCode ==2, "Shrub",
-                                                    ifelse(tblSpeciesGeneric$GrowthHabitCode ==3, "SubShrub",
-                                                           ifelse(tblSpeciesGeneric$GrowthHabitCode ==4, "Succulent",
-                                                                  ifelse(tblSpeciesGeneric$GrowthHabitCode ==5, "Forb",
-                                                                         ifelse(tblSpeciesGeneric$GrowthHabitCode ==6, "Graminoid", "Sedge"))))))
-
-  tblSpeciesGeneric$Noxious <- ""
-  tblSpeciesGeneric$Invasive <- NA
-  tblSpeciesGeneric$UpdatedSpeciesCode <- ""
-  tblSpeciesGeneric$Notes <- ""
-  tblSpeciesGeneric$SpeciesState <- tblSpeciesGeneric$project
-  tblSpeciesGeneric$SG_Group <- ""
-
-  generic_keep <- tblSpeciesGeneric %>% dplyr::select(SpeciesCode,ScientificName, Family, GrowthHabit, GrowthHabitSub, Duration,
-                                                      Noxious, Invasive,UpdatedSpeciesCode, Notes,
-                                                      SpeciesState,SG_Group)
-
-  tblSpecies$GrowthHabit <- ifelse(tblSpecies$GrowthHabitCode %in% woody_codes, "Woody",
-                                   "NonWoody")
-
-  tblSpecies$GrowthHabitSub <- ifelse(tblSpecies$GrowthHabitCode ==1, "Tree",
-                                      ifelse(tblSpecies$GrowthHabitCode ==2, "Shrub",
-                                             ifelse(tblSpecies$GrowthHabitCode ==3, "SubShrub",
-                                                    ifelse(tblSpecies$GrowthHabitCode ==4, "Succulent",
-                                                           ifelse(tblSpecies$GrowthHabitCode ==5, "Forb",
-                                                                  ifelse(tblSpecies$GrowthHabitCode ==6, "Graminoid", "Sedge"))))))
-
-  tblSpecies$Noxious <- ""
-  tblSpecies$Invasive <- ifelse(tblSpecies$Invasive == 0, FALSE,
-                                ifelse(tblSpecies$Invasive == 1, TRUE,  NA ))
-  tblSpecies$UpdatedSpeciesCode <- USDA_plants$UpdatedSpeciesCode[match(tblSpecies$SpeciesCode, USDA_plants$SpeciesCode)]
-  tblSpecies$Notes <- ""
-  tblSpecies$SpeciesState <- tblSpecies$project
-  tblSpecies$SG_Group <- ""
-
-  species_keep <- tblSpecies %>% dplyr::select(SpeciesCode,ScientificName, Family, GrowthHabit, GrowthHabitSub,Duration,
-                                               Noxious, Invasive,UpdatedSpeciesCode, Notes,
-                                               SpeciesState,SG_Group)
- #for correct geoindicator calculations, the species list must have the updated species code in the species code column, having all the attributes described
-  `%notin%` <- Negate(`%in%`)
-  updated_species <- species_keep[!is.na(species_keep$UpdatedSpeciesCode), ]
-  updated_species <- updated_species|> filter(updated_species$UpdatedSpeciesCode %notin% updated_species$SpeciesCode)
-
-  if(nrow(updated_species) > 0){
-    updated_species$SpeciesCode <- updated_species$UpdatedSpeciesCode
-    species_keep <- rbind(species_keep, updated_species)}
-
-  species <- rbind(species_keep, generic_keep)
-
-  for(projkey in projectkey){
-    #path_species <- "D:/data_preparation_docs_used_from_06012024_04302025/data_preparation_docs_used_from_06012024_04302025/Docs for data prep/Data/species_lists/species_"
-    projsp <- species |> subset(species$SpeciesState == projkey)
-    write.csv(projsp, paste0(path_species_main,"species_",  projkey, ".csv"), row.names = F)
-
-  }
-}else{
-  print("User selected that species list is already created, thus not using DIMA files to produce a species list")
-}
-}
-###############################################
-
-
-
-
-
 ################################################
 #' Create header
 #'
@@ -2322,6 +2231,7 @@ db_info <- function(path_foringest, DateLoadedInDb){
 
 }
 ##############################################
+
 
 
 
