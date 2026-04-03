05367b9d0513d349442838847c803b19b0437963
bwheeler-nmsu <97681002+bwheeler-nmsu@users.noreply.github.com>
2026-04-03 15:02:15 -0600

    translate rwuiring data type

--- R/translate.R
+++ R/translate.R
@@ -159,6 +159,7 @@ translate_coremethods2 <- function(path_tall, path_out, path_schema,  verbose = 
     print("Translating height data")
     tall_ht  <- readRDS(file.path(path_tall, "height_tall.Rdata")) |>
       dplyr::left_join(dataHeader |> dplyr::select(PrimaryKey, DateVisited))
+    tall_ht$DateVisited <- as.Date(tall_ht$DateVisited, format = "%Y-%m-%d")
     dataHeight <- tall_ht |>
       translate_schema2(schema = schema,
                         # projectkey = projectkey,
@@ -205,6 +206,8 @@ translate_coremethods2 <- function(path_tall, path_out, path_schema,  verbose = 
     print("Translating canopy gap data")
     tall_gap <- readRDS(file.path(path_tall, "gap_tall.Rdata")) |>
       dplyr::left_join(dataHeader |> dplyr::select(PrimaryKey, DateVisited))
+    tall_gap$DateVisited <- as.Date(tall_gap$DateVisited, format = "%Y-%m-%d")
+
     dataGap <- tall_gap |>
       translate_schema2(schema = schema,
                         #    projectkey = projectkey,
