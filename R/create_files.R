adcad75b591f986a4d10cf5681d92ec042178d40
bwheeler-nmsu <bwheeler@nmsu.edu>
2026-06-17 10:30:35 -0600

    need lh hgt rw

--- R/create_files.R
+++ R/create_files.R
@@ -868,6 +868,7 @@ create_dimatables_RW <- function(dsn, projectkey, path_dimatables, path_tall) {
   colnames(tblLPIDetail)[colnames(tblLPIDetail) == 'EvaluationID'] <- 'PrimaryKey'
   tblLPIDetail$ShrubShape <- NA
   tblLPIDetail$SpeciesLowerHerb <- NA
+  tblLPIDetail$HeightLowerHerb <- NA
   utils::write.csv(tblLPIDetail, file.path(path_dimatables, "tblLPIDetail.csv"), row.names = FALSE)
 
   tblLPIHeader <- lpi %>%
